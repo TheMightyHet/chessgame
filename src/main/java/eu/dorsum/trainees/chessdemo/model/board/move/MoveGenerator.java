b75899103b0ae87c0c5d06ca5d652367c86b03e2
Győrffy Gyula <60112870+TheMightyHet@users.noreply.github.com>
2025-03-19 16:08:59 +0100

    Delete src directory

--- src/main/java/eu/dorsum/trainees/chessdemo/model/board/move/MoveGenerator.java
+++
@@ -1,8 +0,0 @@
-package eu.dorsum.trainees.chessdemo.model.board.move;
-
-import eu.dorsum.trainees.chessdemo.model.board.Piece;
-
-public interface MoveGenerator {
-
-    long getPossibleMovesForPiece(long piecePosition, long allFriendlyPieces, long allEnemyPieces, Piece piece);
-}
