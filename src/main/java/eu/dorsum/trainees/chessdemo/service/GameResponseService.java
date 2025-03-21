b21ab3a734a80779bfd997edfa934260c6cf7743
Győrffy Gyula <60112870+TheMightyHet@users.noreply.github.com>
2025-03-21 10:53:35 +0100

    Delete src directory

--- src/main/java/eu/dorsum/trainees/chessdemo/service/GameResponseService.java
+++
@@ -1,7 +0,0 @@
-package eu.dorsum.trainees.chessdemo.service;
-
-import eu.dorsum.trainees.chessdemo.model.GameResponse;
-
-public interface GameResponseService {
-    GameResponse getGameResponse(Long id);
-}
